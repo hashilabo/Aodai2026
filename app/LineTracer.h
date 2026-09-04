1bc20765b79121038cab57718270baf4ba74230f
haship <haship@aomori-u.ac.jp>
2026-09-04 20:45:39 +0900

    LRコースの切り替えを追加
    ETロボコンシミュレータにおける左右コースの切り替え方について
    
    https://github.com/ETrobocon/etrobo/wiki/sim_ev3_lr_course_change

--- app/LineTracer.h
+++ app/LineTracer.h
@@ -13,22 +13,31 @@
 #include "LineMonitor.h"
 #include "Walker.h"
 
-class LineTracer {
+class LineTracer
+{
 public:
-	static const float Kp;
-	static const int   bias;
-	
-	LineTracer(const LineMonitor* lineMonitor,
-               Walker* walker);
+    static const float Kp;
+    static const int bias;
+
+#if defined(MAKE_RIGHT)
+    static const int _LEFT = 0;
+#define _EDGE 1
+#else
+    static const int _LEFT = 1;
+#define _EDGE -1
+#endif
+
+    LineTracer(const LineMonitor *lineMonitor,
+               Walker *walker);
 
     void run();
 
 private:
-    const LineMonitor* mLineMonitor;
-    Walker* mWalker;
+    const LineMonitor *mLineMonitor;
+    Walker *mWalker;
     bool mIsInitialized;
 
     float calcPropValue(int diffReflection);
 };
 
-#endif  // ETTR_APP_LINETRACER_H_
+#endif // ETTR_APP_LINETRACER_H_
