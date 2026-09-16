63f3268644d14d7ddcbcc64bd5d70586fde6a2a1
haship <haship@gmail.com>
2026-09-16 23:06:30 +0900

    tune01_v1.0

--- unit/Walker.h
+++ unit/Walker.h
@@ -14,8 +14,8 @@
 
 class Walker {
 public:
-    static const int RIGHT_TURN = -30;  //¶ƒ^[ƒ“‚Ì§Œä’l
-    static const int LEFT_TURN = 30;    //‰Eƒ^[ƒ“‚Ì§Œä’l
+    static const int RIGHT_TURN = -30;  //ï¿½ï¿½ï¿½^ï¿½[ï¿½ï¿½ï¿½ï¿½ï¿½Ìï¿½ï¿½ï¿½l
+    static const int LEFT_TURN = 30;    //ï¿½Eï¿½^ï¿½[ï¿½ï¿½ï¿½ï¿½ï¿½Ìï¿½ï¿½ï¿½l
   
     Walker(spikeapi::Motor& leftWheel,
                     spikeapi::Motor& rightWheel);
@@ -30,7 +30,7 @@ private:
     int mTurn;
 
   #ifndef MAKE_RASPIKE
-    const int8_t pwm = 30;
+    const int8_t pwm = 45;
   #else
     const int8_t pwm = 40;
   #endif
